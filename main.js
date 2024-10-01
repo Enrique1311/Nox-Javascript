62ac5243eff113d4a7fe6c232f4fcc8be2f885ae
Enrique1311 <ejspinelli131176@gmail.com>
2024-10-01 14:48:13 -0300

    SEO meta tags were added.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const d = document;
 
 // * Mobile Menu ****************************************************
+
 const $menuIcon = document.querySelector(".menu-icon i"),
 	$mobileNavbarBack = document.querySelector(".mobile-navbar-back"),
 	$mobileNavbar = document.querySelector(".mobile-navbar"),
@@ -16,5 +17,24 @@ const closeMenu = () => {
 	$mobileNavbarBack.classList.remove("opened-menu");
 };
 
+$mobileNavbarBack.addEventListener("click", closeMenu);
 $menuIcon.addEventListener("click", openMenu);
 $closeMenuIcon.addEventListener("click", closeMenu);
+
+// * Top Button ********************************************************
+
+window.addEventListener("scroll", (e) => {
+	let scrollTop = d.documentElement.scrollTop;
+
+	if (scrollTop > 100) {
+		d.querySelector(".top-btn").classList.remove("hidden-btn");
+	} else {
+		d.querySelector(".top-btn").classList.add("hidden-btn");
+	}
+});
+
+d.addEventListener("click", (e) => {
+	if (e.target.matches(".top-btn") || e.target.matches(".top-btn i")) {
+		window.scrollTo({ behavior: "smooth", top: 0 });
+	}
+});
